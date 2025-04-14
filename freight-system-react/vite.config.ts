75c896b7e7ec4856c8fdcc3916e7fcd3b393c5fc
sunqiquan <sunqq2008@qq.com>
2025-04-14 22:08:00 +0800

    init freight-system-react  project and create file directory

---
+++ freight-system-react/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": "http://api-driver.marsview.cc",
+    },
+  },
+});
